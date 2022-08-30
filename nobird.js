5c733f6d08c644b6c5de207f19f1e5a3680670d8
ethrythedev <58019814+ethrythedev@users.noreply.github.com>
2022-08-31 08:01:08 +1200

    Update nobird.js

--- nobird.js
+++ nobird.js
@@ -3,4 +3,4 @@
 */
 console.log("twitter success");
 if(window.location.hash == "#elon") return;
-document.body.innerHTML = "<img src='./img/musk.png'><a href='#elon'>Allow for now</a>";
+document.body.innerHTML = "<img src='https://cdn.jsdelivr.net/gh/ethrythedev/AntiTwitter@latest/img/musk.png'><a href='#elon'>Allow for now</a>"; // probably won't work, someone please add to twitter cdn
