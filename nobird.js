e09141a12827b86ef74f5b908e1f3be968b12d32
ethrythedev <58019814+ethrythedev@users.noreply.github.com>
2022-08-31 07:39:03 +1200

    Add files via upload

---
+++ nobird.js
@@ -0,0 +1,6 @@
+/*
+    Inserts message on twitter.com
+*/
+console.log("twitter success");
+if(window.location.hash == "#elon") return;
+document.body.innerHTML = "<img src='./img/musk.png'><a href='#elon'>Allow for now</a>";
