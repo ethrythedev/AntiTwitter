76c59b2a1cf314927087b9a55c69b2a9bfbad273
ethrythedev <58019814+ethrythedev@users.noreply.github.com>
2022-08-31 19:09:38 +1200

    Fixed image not showing up

--- nobird.js
+++ nobird.js
@@ -2,5 +2,7 @@
     Inserts message on twitter.com
 */
 console.log("twitter success");
-if(window.location.hash == "#elon") return;
-document.body.innerHTML = "<img src='https://cdn.jsdelivr.net/gh/ethrythedev/AntiTwitter@latest/img/musk.png'><a href='#elon'>Allow for now</a>"; // probably won't work, someone please add to twitter cdn
+if(window.location.hash == "#elon") {
+} else {
+  document.body.innerHTML = "<img src='https://pbs.twimg.com/media/FbeJvRRaAAI9twL?format=png&name=360x360'><p><a style='font: sans-serif; background: white; padding: 15px; border-radius: 10px; color: black; text-decoration: none;' href='#elon'>Allow for now (reload after)</a></p>"; 
+}
